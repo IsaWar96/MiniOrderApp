3c623402a2d46ce0352408a5d055c61fb8d57af6
Isak W <isakwarsame3@gmail.com>
2025-11-20 22:05:37 +0100

    Add basic validation and properties to Return entity

--- MiniOrderApp.Domain/Return.cs
+++ MiniOrderApp.Domain/Return.cs
@@ -2,5 +2,26 @@
 
 public class Return
 {
+    public int Id { get; set; }
+    public int OrderId { get; private set; }
+    public DateTime ReturnDate { get; private set; }
+    public string Reason { get; private set; } = "";
+    public decimal RefundedAmount { get; private set; }
 
+    public Return(int orderId, DateTime returnDate, string reason, decimal refundedAmount)
+    {
+        if (orderId <= 0)
+            throw new ArgumentException("OrderId must be positive", nameof(orderId));
+        if (refundedAmount < 0)
+            throw new ArgumentException("Refunded amount cannot be negative", nameof(refundedAmount));
+
+        OrderId = orderId;
+        ReturnDate = returnDate;
+        Reason = reason ?? "";
+        RefundedAmount = refundedAmount;
+    }
+    // For Dapper
+    public Return()
+    {
+    }
 }
