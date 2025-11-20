85324a9b29a72adb6c7bbc6ce84af3a3d2cf772d
Isak W <isakwarsame3@gmail.com>
2025-11-20 23:50:07 +0100

    Console App listing orders

--- MiniOrderApp.Presentation/Program.cs
+++ MiniOrderApp.Presentation/Program.cs
@@ -1,10 +1,27 @@
 ﻿using MiniOrderApp.Infrastructure.Database;
 using MiniOrderApp.Infrastructure.Repositories;
+using MiniOrderApp.Domain.Interfaces;
+using MiniOrderApp.Domain;
 internal class Program
 {
     private static void Main(string[] args)
     {
-        var conn = new SQLiteConnectionFactory("Data Source=miniorder.db");
-        var orderRepo = new OrderRepository(conn);
+        var connectionstring = "Data Source=miniorder.db";
+
+        DbInitializer.Initialize(connectionstring);
+
+
+        var factory = new SQLiteConnectionFactory(connectionstring);
+        IOrderRepository orderRepo = new OrderRepository(factory);
+
+        var orders = orderRepo.GetOrders();
+
+        foreach (var o in orders)
+        {
+            Console.WriteLine($"Order {o.Id} - Customer: {o.CustomerId} - Total: {o.TotalAmount}");
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
     }
 }
