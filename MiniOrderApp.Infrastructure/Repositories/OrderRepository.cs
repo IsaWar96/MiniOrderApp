55167d6de5cf9bdfcc184c3179dc839743c815f1
Isak W <isakwarsame3@gmail.com>
2025-11-20 23:49:40 +0100

    Implement OrderRepository method GetOrders using Dapper

--- MiniOrderApp.Infrastructure/Repositories/OrderRepository.cs
+++ MiniOrderApp.Infrastructure/Repositories/OrderRepository.cs
@@ -1,6 +1,8 @@
 using MiniOrderApp.Domain;
 using MiniOrderApp.Domain.Interfaces;
 using MiniOrderApp.Infrastructure.Database;
+using Dapper;
+using System.Data;
 
 namespace MiniOrderApp.Infrastructure.Repositories;
 
@@ -10,6 +12,22 @@ public class OrderRepository : IOrderRepository
     public OrderRepository(SQLiteConnectionFactory factory)
     {
         _factory = factory;
+    }
+    public IEnumerable<Order> GetOrders()
+    {
+        using IDbConnection db = _factory.Create();
+
+        const string sql = @"
+        SELECT 
+            OrderId    AS Id,
+            CustomerId,
+            OrderDate,
+            Status,
+            TotalAmount
+        FROM Orders;
+        ";
+
+        return db.Query<Order>(sql);
     }
     public void Add(Order order)
     {
@@ -26,10 +44,6 @@ public class OrderRepository : IOrderRepository
         throw new NotImplementedException();
     }
 
-    public IEnumerable<Order> GetOrders()
-    {
-        throw new NotImplementedException();
-    }
 
     public void Update(Order order)
     {
