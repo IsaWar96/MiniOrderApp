4ccdc3528d84fb5b0150a663b1766af6c9dde128
Isak W <isakwarsame3@gmail.com>
2025-11-21 16:15:56 +0100

    Minor fix to nullabilty issue

--- MiniOrderApp.Domain/Interfaces/IReturnRepository.cs
+++ MiniOrderApp.Domain/Interfaces/IReturnRepository.cs
@@ -3,6 +3,6 @@ namespace MiniOrderApp.Domain.Interfaces;
 public interface IReturnRepository
 {
     void AddReturn(Return returnInfo);
-    Return GetByOrderId(int id);
+    Return? GetByOrderId(int id);
 }
 
