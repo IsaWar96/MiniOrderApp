300089c8b54afb227607ec4ed2302e22c55a4163
Isak W <isakwarsame3@gmail.com>
2025-11-21 18:53:01 +0100

    Simplify program.cs by creating folder UI

--- MiniOrderApp.Presentation/Program.cs
+++ MiniOrderApp.Presentation/Program.cs
@@ -1,36 +1,23 @@
-﻿using MiniOrderApp.Domain;
-using MiniOrderApp.Domain.Interfaces;
-using MiniOrderApp.Infrastructure.Database;
+﻿using MiniOrderApp.Infrastructure.Database;
 using MiniOrderApp.Infrastructure.Repositories;
-using MiniOrderApp.Infrastructure.Services;
+using MiniOrderApp.Presentation.UI;
+using MiniOrderApp.Domain.Interfaces;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
-
         var connectionString = "Data Source=miniorder.db";
 
         DbInitializer.Initialize(connectionString);
 
         var factory = new SQLiteConnectionFactory(connectionString);
-        // ICustomerRepository customerRepo = new CustomerRepository(factory);
+
+        ICustomerRepository customerRepo = new CustomerRepository(factory);
         IOrderRepository orderRepo = new OrderRepository(factory);
+        IReturnRepository returnRepo = new ReturnRepository(factory);
 
-        var orders = orderRepo.GetOrders().ToList();
-        foreach (var o in orders)
-        {
-            Console.WriteLine($"{o.Id}. Customer {o.CustomerId}, Status: {o.Status}, Total: {o.TotalAmount}");
-        }
-
-        Console.Write("Enter ID on order you want to delete: ");
-        var orderIdText = Console.ReadLine();
-        if (!int.TryParse(orderIdText, out var orderId))
-        {
-            Console.WriteLine("Invalid ID.");
-            return;
-        }
-
-        orderRepo.Delete(orderId);
-        Console.WriteLine("Order deleted.");
+        var menu = new MainMenu(customerRepo, orderRepo, returnRepo);
+        menu.Start();
     }
 }
