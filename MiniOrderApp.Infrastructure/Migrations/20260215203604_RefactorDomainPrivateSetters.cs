92a852b90112350c1b8b1f574bffb620fe139cb6
Isak W <isakwarsame3@gmail.com>
2026-02-15 21:37:24 +0100

    Add database migration for domain entity refactoring (no schema changes)

---
+++ MiniOrderApp.Infrastructure/Migrations/20260215203604_RefactorDomainPrivateSetters.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniOrderApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorDomainPrivateSetters : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
